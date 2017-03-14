8233a7c258f61a2954db135c432448c814c97412
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-03-14 14:26:28 -0400

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friends = [
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    name:"Ahmed",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,18 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
+
+    for (var i = 0; i < friends.length; i++) {
+      var count = 0;
+      for (var index = 0; index < 10; index++) {
+        count += math.abs(friends[i].scores[index] - req.body.scores[index]);
+      }
+      console.log(count);
+    }
+
     friends.push(req.body);
+
+
   });
 
-};
+};//exports

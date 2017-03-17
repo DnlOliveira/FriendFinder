e8848bbb1c4a3c680ac10d03ec33070400c63eb6
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-03-17 13:17:54 -0400

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,23 @@
 var friends = [
   {
-    name:"Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores:[1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    name:"John Snow",
+    photo:"http://res.cloudinary.com/nylon/image/upload/t_posts-max,f_auto,fl_lossy,q_80/posts/facebook_thumbnails/000/020/878/jon-snow.jpg",
+    scores:[5, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+  },
+  {
+    name:"Daenarys Targaryen",
+    photo:"http://i.lv3.hbo.com/assets/images/series/game-of-thrones/character/s5/daenarys-1024.jpg",
+    scores:[3, 4, 2, 2, 2, 5, 1, 4, 2, 4]
+  },
+  {
+    name:"Cersi Lanninster",
+    photo:"http://i.huffpost.com/gen/1205630/images/o-CERSEI-DOUTZEN-facebook.jpg",
+    scores:[3, 4, 4, 2, 1, 1, 2, 5, 4, 3]
+  },
+  {
+    name:"Jamie Lanninster",
+    photo:"http://vignette4.wikia.nocookie.net/gameofthrones/images/9/96/Jaime_Lannister_in_The_Laws_of_Gods_and_Men.png",
+    scores:[2, 2, 2, 4, 4, 4, 3, 3, 3, 1]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,17 +8,32 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
 
+    var lowestDiff = 100;
+    var match = [];
+    //loop to compare scores of user
+    //with the rest of the users already saved
     for (var i = 0; i < friends.length; i++) {
+
       var count = 0;
       for (var index = 0; index < 10; index++) {
-        count += math.abs(friends[i].scores[index] - req.body.scores[index]);
+        count += Math.abs(friends[i].scores[index] - req.body.scores[index]);
       }
-      console.log(count);
-    }
 
+      console.log(count);
+      if (count <= lowestDiff){
+        lowestDiff = count;
+        match[0] = friends[i];
+      }
+
+    }//for-loop
+
+    //pushes newly entered person into list of users to be compared
     friends.push(req.body);
 
+    res.json(match);
+    console.log(match);
 
-  });
+
+  });//app.post
 
 };//exports

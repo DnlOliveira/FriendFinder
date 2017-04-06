e446735710dad3e72bd2cc00b7370de4b52a766f
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-04-06 00:54:54 -0400

    bug fixes

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,14 +10,39 @@ var friends = [
     scores:[3, 4, 2, 2, 2, 5, 1, 4, 2, 4]
   },
   {
-    name:"Cersi Lanninster",
+    name:"Cersi Lannister",
     photo:"http://i.huffpost.com/gen/1205630/images/o-CERSEI-DOUTZEN-facebook.jpg",
     scores:[3, 4, 4, 2, 1, 1, 2, 5, 4, 3]
   },
   {
-    name:"Jamie Lanninster",
+    name:"Jamie Lannister",
     photo:"http://vignette4.wikia.nocookie.net/gameofthrones/images/9/96/Jaime_Lannister_in_The_Laws_of_Gods_and_Men.png",
     scores:[2, 2, 2, 4, 4, 4, 3, 3, 3, 1]
+  },
+  {
+    name:"Robb Stark",
+    photo:"https://s-media-cache-ak0.pinimg.com/736x/1b/ac/b0/1bacb0c2f0fee0185a955fbf851bce20.jpg",
+    scores:[2, 3, 2, 3, 5, 5, 4, 5, 2, 1]
+  },
+  {
+    name:"Theon Greyjorn",
+    photo:"https://images.8tracks.com/cover/i/009/274/214/gameofthrones14122-1401745965-6151.jpg?rect=0,0,450,450&q=98&fm=jpg&fit=max",
+    scores:[3, 5, 3, 5, 4, 3, 2, 1, 3, 4]
+  },
+  {
+    name:"Sansa Stark",
+    photo:"http://www.radiotimes.com/uploads/images/Original/108900.jpg",
+    scores:[2, 2, 3, 3, 4, 4, 5, 5, 1, 1]
+  },
+  {
+    name:"Margaery Tyrell",
+    photo:"https://pbs.twimg.com/profile_images/713228404980523008/LjZqdtoP.jpg",
+    scores:[5, 4, 1, 2, 3, 4, 2, 3, 1, 5]
+  },
+  {
+    name:"Tyrion Lannister",
+    photo:"https://pbs.twimg.com/profile_images/668279339838935040/8sUE9d4C.jpg",
+    scores:[3, 3, 3, 4, 4, 4, 1, 2, 3, 5]
   }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,34 +2,45 @@ var friends = require("../data/friends");
 
 module.exports = function(app) {
 
+  //retrieves objects from array of users
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
+  //posts new user into array of users &
+  //decides best match based on lowest difference in answers
   app.post("/api/friends", function(req, res) {
 
-    var lowestDiff = 100;
-    var match = [];
-    //loop to compare scores of user
-    //with the rest of the users already saved
+    const numOfQuestions = 10;
+    var match = []; //holds matched friend
+
+    //loop to go through all users
     for (var i = 0; i < friends.length; i++) {
 
       var count = 0;
-      for (var index = 0; index < 10; index++) {
+      //loop to go through user's answers
+      for (var index = 0; index < numOfQuestions; index++) {
         count += Math.abs(friends[i].scores[index] - req.body.scores[index]);
       }
-
       console.log(count);
-      if (count <= lowestDiff){
-        lowestDiff = count;
-        match[0] = friends[i];
+
+      //if an user's score is lower than the previous user's
+      //then set him as a match or else keep the previous
+      //the match (multiple users with same scores can be matches)
+      if (i == 0 || count == match[0]) {
+        match.push(friends[i]);
+      } else if (count < match[0]){
+        match.length = 0;
+        match.push(friends[i]);
       }
 
     }//for-loop
 
     //pushes newly entered person into list of users to be compared
+    //for next survey filled
     friends.push(req.body);
 
+    //returns matched user
     res.json(match);
     console.log(match);
 
